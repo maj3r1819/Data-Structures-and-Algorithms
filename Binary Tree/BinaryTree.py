73c501aa1274eb66cfa0e3083651d139c731aab7
Sagar <major1819@gmail.com>
2021-04-28 14:52:14 +0530

    Level Order Traversal added

--- Binary Tree/BinaryTree.py
+++ Binary Tree/BinaryTree.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(0, 'D:\Projects\Data-Structures-and-Algorithms\Queue')
+from QueueLinkedList import Queue
 class TreeNode:
     def __init__(self,data):
         self.data = data
@@ -34,9 +37,28 @@ def postOrderTraversal(rootNode):
     postOrderTraversal(rootNode.rightChild)
     print(rootNode.data)
 
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        customqueue = Queue()
+        customqueue.enqueue(rootNode)
+        while not (customqueue.isempty()):
+            root = customqueue.dequeue()
+            print(root.value.data)
+            if (root.value.leftChild is not None):
+                customqueue.enqueue(root.value.leftChild)
+
+            if (root.value.rightChild is not None):
+                customqueue.enqueue(root.value.rightChild)
+
+
+
 print("Pre Order Traversal: ")
 preOrderTraversal(newBT)
 print("In Order Traversal: ")
 inOrderTraversal(newBT)
 print("Post Order Traversal: ")
 postOrderTraversal(newBT)
+print("Level Order Traversal: ")
+levelOrderTraversal(newBT)
