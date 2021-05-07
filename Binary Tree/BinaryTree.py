8ff9834a9ba0e7e75e1f5e760ffaf4b97c28a586
Sagar <major1819@gmail.com>
2021-05-07 13:16:05 +0530

    all binary tree operations added

--- Binary Tree/BinaryTree.py
+++ Binary Tree/BinaryTree.py
@@ -74,7 +74,6 @@ def searchTree(rootNode, nodeValue):
                 customqueue.enqueue(root.value.rightChild)
         return "Not Found"
 
-
 def insertNodeBT(rootNode, newNode):
     if not rootNode:
         rootNode = newNode
@@ -97,10 +96,71 @@ def insertNodeBT(rootNode, newNode):
                 root.value.rightChild = newNode
                 return "Successfully Inserted"
 
+def getDeepestNode(rootNode):
+    if not rootNode:
+        return
+
+    else:
+        customqueue = Queue()
+        customqueue.enqueue(rootNode)
+        while not(customqueue.isempty()):
+            root = customqueue.dequeue()
+            if (root.value.leftChild is not None):
+                customqueue.enqueue(root.value.leftChild)
+
+            if (root.value.rightChild is not None):
+                customqueue.enqueue(root.value.rightChild)
+
+        deepestNode = root.value
+        return deepestNode
+
+def deleteDeepestNode(rootNode, deepestNode):
+    if not rootNode:
+        return
+    else:
+        customqueue = Queue()
+        customqueue.enqueue(rootNode)
+        while not(customqueue.isempty()):
+            root = customqueue.dequeue()
+            if root.value == deepestNode:
+                root.value = None
+                return
+            if root.value.rightChild:
+                if root.value.rightChild ==deepestNode:
+                    root.value.rightChild = None
+                    return
+                else:
+                    customqueue.enqueue(root.value.rightChild)
+
+            if root.value.leftChild:
+                if root.value.leftChild ==deepestNode:
+                    root.value.leftChild = None
+                    return
+                else:
+                    customqueue.enqueue(root.value.leftChild)
 
 
+def deleteNode(rootNode, node):
+    if not rootNode:
+        return
+    else:
+        customqueue = Queue()
+        customqueue.enqueue(rootNode)
+        while not(customqueue.isempty()):
+            root = customqueue.dequeue()
+            if root.value.data == node:
+                deepestNode = getDeepestNode(rootNode)
+                root.value.data = deepestNode.data
+                deleteDeepestNode(rootNode,deepestNode)
+                return "Node has been successfully deleted"
 
+            if (root.value.leftChild is not None):
+                customqueue.enqueue(root.value.leftChild)
 
+            if (root.value.rightChild is not None):
+                customqueue.enqueue(root.value.rightChild)
+
+        return "fail"
 
 
 print("Pre Order Traversal: ")
@@ -118,5 +178,14 @@ newnode = TreeNode("Cola")
 insertNodeBT(newBT, newnode)
 levelOrderTraversal(newBT)
 
+# print("Deepest node is :")
+# deepestNode  = getDeepestNode(newBT)
+# print(deepestNode.data)
+# print("Level order traversal after deleting last node :")
+# deleteDeepestNode(newBT, deepestNode)
+# levelOrderTraversal(newBT)
 
+print("Deleting given node in a bt:")
+deleteNode(newBT, "Hot")
+levelOrderTraversal(newBT)
 
