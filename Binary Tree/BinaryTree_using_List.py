b59be17f1b716b7bf1c01f9f8da9ee147b638776
Sagar <major1819@gmail.com>
2021-05-16 19:18:46 +0530

    Node insertion and search

--- Binary Tree/BinaryTree_using_List.py
+++ Binary Tree/BinaryTree_using_List.py
@@ -5,4 +5,22 @@ class BinaryTree:
         self.maxSize = size
 
 
+    def insertNode(self, value):
+        if self.lastUsedIndex +1 == self.maxSize:
+            return "Binary Tree is Full"
+        self.customList[self.lastUsedIndex+1] = value
+        self.lastUsedIndex+=1
+        return "The Value has been inserted"
+
+    def searchNode(self, nodeValue):
+        for i in range(len(self.customList)):
+            if self.customList[i] == nodeValue:
+                return "Success"
+        return "Not found"
+
 newBT = BinaryTree(8)
+newBT.insertNode("Drinks")
+newBT.insertNode("Hot")
+newBT.insertNode("Cold")
+print(newBT.searchNode("Cold"))
+
