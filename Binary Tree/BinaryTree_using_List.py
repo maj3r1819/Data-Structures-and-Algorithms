19371eb99db5be048e74930f7073d4be9b28e8fe
Sagar <major1819@gmail.com>
2021-05-16 19:48:02 +0530

    Preorder Traversal

--- Binary Tree/BinaryTree_using_List.py
+++ Binary Tree/BinaryTree_using_List.py
@@ -18,9 +18,18 @@ class BinaryTree:
                 return "Success"
         return "Not found"
 
+    def preOrderTraversal(self,index):
+        if index > self.lastUsedIndex:
+            return
+        print(self.customList[index])
+        self.preOrderTraversal(index*2)
+        self.preOrderTraversal(index*2 + 1)
+
 newBT = BinaryTree(8)
 newBT.insertNode("Drinks")
 newBT.insertNode("Hot")
 newBT.insertNode("Cold")
+newBT.insertNode("Tea")
+newBT.insertNode("Coffee")
 print(newBT.searchNode("Cold"))
-
+newBT.preOrderTraversal(1)
