1dfc9dae8f9b25ae85bf161eddad539cb51e73ff
Sagar <major1819@gmail.com>
2021-06-15 00:14:05 +0530

    started sorting algorithms

---
+++ Sorting Algorithms/BubbleSort.py
@@ -0,0 +1,20 @@
+def bubbleSort(customList):
+    for i in range(len(customList)-1):
+        for j in range(len(customList)-i-1):
+            if customList[j] > customList[j+1]:
+                customList[j], customList[j+1] = customList[j+1], customList[j]
+
+    print(customList)
+
+
+clist = [5,4,3,2,1]
+bubbleSort(clist)
+
+"""
+When to use Bubble Sort?
+-Space is a concern
+-Easy to Implement
+
+WHen to avoid?
+-time complexity is very high O(n^2). so cannot use where time is an issue
+"""
